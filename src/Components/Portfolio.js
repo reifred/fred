8ba9d1cfcdc9ac32e15e220c987bae88697daa9a
reifred <reifred33@gmail.com>
2019-12-20 00:55:06 +0300

    Update some urls and images change grid to three columns

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -9,18 +9,30 @@ class Portfolio extends Component {
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
+               <img className="img-responsive" alt={projects.title} src={projectImage} />
             </a>
           </div>
           <div className="row-flex">
             <div className="col">
-              <a href={projects.backend}>Backend</a>
+              {
+                projects.backend === "#private"
+                ? <h6>Backend</h6>
+                : <a href={projects.backend}>Backend</a>
+              }
             </div>
             <div className="col">
-              <a href={projects.frontend}>Frontend</a>
+              {
+                projects.frontend === "#private"
+                ? <h6>Frontend</h6>
+                : <a href={projects.frontend}>Frontend</a>
+              }
             </div>
             <div className="col">
-              <a href={projects.site_url}>App</a>
+              {
+                projects.site_url === "#private"
+                ? <h6>Not hosted</h6>
+                : <a href={projects.site_url}>App</a>
+              }
             </div>
           </div>
         </div>
@@ -36,7 +48,7 @@ class Portfolio extends Component {
 
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="bgrid-thirds cf">
                 {projects}
             </div>
           </div>
